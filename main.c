13983aa2d6dadabd39df147a30e9889a123ba68a
LuChristCho <masihsheikhii@gmail.com>
2025-04-11 23:55:43 +0330

    debug wifi name

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <windows.h>
+#include <stdio.h>
+#include <string.h>
 
-void setDNS();
+void setDNS(const char* primaryDNS, const char* secondaryDNS);
 void resetDNS();
 void updateLog(HWND hwnd, const char *message);
+int readDNSFromFile(char* primaryDNS, char* secondaryDNS, size_t bufferSize);
+
+#define MAX_DNS_LENGTH 16
+#define CONFIG_FILE "dns_config.txt"
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     static HWND logBox;
@@ -16,11 +22,34 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
             break;
         case WM_COMMAND:
             if (LOWORD(wParam) == 1) {
-                setDNS();
-                updateLog(logBox, "DNS set to 178.22.122.100 and 185.51.200.2");
+                char primaryDNS[MAX_DNS_LENGTH] = {0};
+                char secondaryDNS[MAX_DNS_LENGTH] = {0};
+                int useDefault = 0;
+                
+                if (!readDNSFromFile(primaryDNS, secondaryDNS, MAX_DNS_LENGTH)) {
+                    useDefault = 1;
+                    strncpy(primaryDNS, "178.22.122.100", MAX_DNS_LENGTH);
+                    strncpy(secondaryDNS, "185.51.200.2", MAX_DNS_LENGTH);
+                }
+                
+                setDNS(primaryDNS, secondaryDNS);
+                
+                char logMessage[256];
+                if (secondaryDNS[0] != '\0') {
+                    snprintf(logMessage, sizeof(logMessage), 
+                             "DNS set to %s (Primary) and %s (Secondary)%s", 
+                             primaryDNS, secondaryDNS,
+                             useDefault ? " (using default values)" : "");
+                } else {
+                    snprintf(logMessage, sizeof(logMessage), 
+                             "DNS set to %s (Primary)%s", 
+                             primaryDNS,
+                             useDefault ? " (using default values)" : "");
+                }
+                updateLog(logBox, logMessage);
             } else if (LOWORD(wParam) == 2) {
                 resetDNS();
-                updateLog(logBox, "DNS reset to default");
+                updateLog(logBox, "DNS reset to default (DHCP)");
             }
             break;
         case WM_DESTROY:
@@ -49,9 +78,16 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     return msg.wParam;
 }
 
-void setDNS() {
-    system("netsh interface ip set dns name=\"Wi-Fi\" static 178.22.122.100");
-    system("netsh interface ip add dns name=\"Wi-Fi\" 185.51.200.2 index=2");
+void setDNS(const char* primaryDNS, const char* secondaryDNS) {
+    char command[256];
+    
+    snprintf(command, sizeof(command), "netsh interface ip set dns name=\"Wi-Fi\" static %s", primaryDNS);
+    system(command);
+    
+    if (secondaryDNS[0] != '\0') {
+        snprintf(command, sizeof(command), "netsh interface ip add dns name=\"Wi-Fi\" %s index=2", secondaryDNS);
+        system(command);
+    }
 }
 
 void resetDNS() {
@@ -64,4 +100,28 @@ void updateLog(HWND hwnd, const char *message) {
     strcat(buffer, message);
     strcat(buffer, "\r\n");
     SetWindowText(hwnd, buffer);
+}
+
+int readDNSFromFile(char* primaryDNS, char* secondaryDNS, size_t bufferSize) {
+    FILE* file = fopen(CONFIG_FILE, "r");
+    
+    if (!file) {
+        return 0;
+    }
+    
+    if (!fgets(primaryDNS, bufferSize, file)) {
+        fclose(file);
+        return 0;
+    }
+    
+    primaryDNS[strcspn(primaryDNS, "\r\n")] = '\0';
+    
+    if (fgets(secondaryDNS, bufferSize, file)) {
+        secondaryDNS[strcspn(secondaryDNS, "\r\n")] = '\0';
+    } else {
+        secondaryDNS[0] = '\0';
+    }
+    
+    fclose(file);
+    return 1;
 }
